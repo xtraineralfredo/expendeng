65903877fe19f658482ac28b738df51c7fecae52
Michael Renteria <mrenteria90@gmail.com>
2019-04-08 22:11:31 -0500

    New Login System, Simple Tables

--- eng_project_expend/settings.py
+++ eng_project_expend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'expend',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+LOGIN_URL = '/login'
--- eng_project_expend/urls.py
+++ eng_project_expend/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.contrib.auth.decorators import login_required, permission_required
+from django.urls import path, include
+from expend.views import SourceList
 
 urlpatterns = [
+    path('',include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
+    path('sources/', login_required(SourceList.as_view()))
 ]
